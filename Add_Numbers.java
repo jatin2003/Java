a5c528ffd2a17acb0c6e56eb55b71af3ccb4cf14
jatinnr <56562571+jatinnr@users.noreply.github.com>
2019-11-13 15:24:04 +0530

    Add Numbers
    Program to add two numbers

---
+++ Add_Numbers.java
@@ -0,0 +1,11 @@
+// Program to Add two numbers
+
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+        
+        int first = 10;
+        int second = 20;
+        int sum = first + second;
+        System.out.println("Sum : " + sum);
+    }
+}
